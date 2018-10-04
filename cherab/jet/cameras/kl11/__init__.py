cc3d69024dd96a0acc785f77f208aa3afcacb2c9
mattngc <matthewncarr@gmail.com>
2018-10-04 17:04:13 +0100

    Bringing KL11 library files into line.

--- cherab/jet/cameras/kl11/__init__.py
+++ cherab/jet/cameras/kl11/__init__.py
@@ -1,2 +1,2 @@
 
-from .load_kl11 import load_kl11_camera, load_kl11_inversion_grid, load_kl11_sensitivity_matrix
+from .load_kl11 import load_kl11_camera, load_kl11_voxel_grid, load_kl11_sensitivity_matrix
--- cherab/jet/cameras/kl11/load_kl11.py
+++ cherab/jet/cameras/kl11/load_kl11.py
@@ -1,9 +1,12 @@
 
 import os
+import json
 import numpy as np
 
+from raysect.core import Point2D
 from raysect.optical.observer import PowerPipeline2D, VectorCamera
-from cherab.tools.observers import load_calcam_calibration, load_inversion_grid
+from cherab.tools.observers import load_calcam_calibration
+from cherab.tools.inversions import ToroidalVoxelGrid
 
 
 def load_kl11_camera(parent=None, pipelines=None, reduction_factor=1):
@@ -24,11 +27,25 @@ def load_kl11_camera(parent=None, pipelines=None, reduction_factor=1):
     return camera
 
 
-def load_kl11_inversion_grid():
+def load_kl11_voxel_grid(parent=None, name=None):
 
     directory = os.path.split(__file__)[0]
+    voxel_grid_file = os.path.join(directory, "kl11_voxel_grid.json")
 
-    return load_inversion_grid(os.path.join(directory, "kl11_inversion_grid.json"))
+    with open(voxel_grid_file, 'r') as fh:
+        grid_description = json.load(fh)
+
+    voxel_coordinates = []
+    for voxel in grid_description['cells']:
+        v1 = Point2D(voxel['p1'][0], voxel['p1'][1])
+        v2 = Point2D(voxel['p2'][0], voxel['p2'][1])
+        v3 = Point2D(voxel['p3'][0], voxel['p3'][1])
+        v4 = Point2D(voxel['p4'][0], voxel['p4'][1])
+        voxel_coordinates.append((v1, v2, v3, v4))
+
+    voxel_grid = ToroidalVoxelGrid(voxel_coordinates, parent=parent, name=name)
+
+    return voxel_grid
 
 
 def load_kl11_sensitivity_matrix(reflections=True):
