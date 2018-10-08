230be2e43e0bc05e3bf630a98ca1a585aadfe96e
Lovell, Jack J <Jack.Lovell@ukaea.uk>
2018-10-08 15:37:20 +0100

    Add support for KB1 overview bolometers
    KB1 has 4 single-channel vertically-viewing bolometers, at 4
    different toroidal locations. It is potentially a useful system
    for studying toroidal asymmetries, but as so far been chronically
    neglected in analysis.

---
+++ demos/diagnostic/kb1/calculate_sensitivities.py
@@ -0,0 +1,30 @@
+
+import time
+import os
+import numpy as np
+from raysect.optical import World
+from raysect.optical.material import AbsorbingSurface
+from raysect.core.workflow import MulticoreEngine
+
+from cherab.jet.machine import import_jet_mesh
+from cherab.jet.bolometry import load_kb1_camera, load_kb1_voxel_grid
+
+# Not enough memory for many processes with big grid, so keep this low
+NCORES = int(os.environ.get("NSLOTS", 4))
+
+
+world = World()
+voxel_grid = load_kb1_voxel_grid(parent=world, name="KB1 voxel grid")
+import_jet_mesh(world, override_material=AbsorbingSurface())
+
+# Calculate KB1 camera sensitivities
+kb1 = load_kb1_camera(parent=world)
+sensitivity_matrix = np.zeros((len(kb1), voxel_grid.count))
+for i, detector in enumerate(kb1):
+    start_time = time.time()
+    print("Calculating detector '{}'...".format(detector.name))
+    detector.render_engine = MulticoreEngine(NCORES)
+    sensitivities = detector.calculate_sensitivity(voxel_grid, ray_count=1000000)
+    sensitivity_matrix[i, :] = sensitivities
+    print("Traced detector '{}' with run time - {:.2G}mins".format(detector.name, (time.time() - start_time) / 60))
+np.save("kb1_sensitivities.npy", sensitivity_matrix)
