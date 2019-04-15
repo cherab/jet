fc6bcce90f1e3cbbe7a41e982ef33e6b2fb6564a
mattngc <matthewncarr@gmail.com>
2019-04-15 13:19:42 +0100

    Edge2D simulation script for KB5.

---
+++ demos/diagnostic/kb5/edge2d_to_kb5.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from raysect.core import Point3D, Vector3D
+from raysect.core.math.function.function3d import PythonFunction3D
+from raysect.optical import Spectrum, World
+from raysect.optical.material import AbsorbingSurface
+from raysect.primitive import import_obj
+
+from cherab.openadas import OpenADAS
+from cherab.core.model import TotalRadiatedPower
+from cherab.edge2d import load_edge2d_from_eproc
+from cherab.jet.machine import import_jet_mesh
+from cherab.jet.machine.cad_files import KB5V, KB5H
+from cherab.jet.bolometry import load_kb5_camera
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('sim_path', help='The path to the edge2D simulation tran folder.')
+parser.add_argument('-o', '--output', help='The name of an output file to which the data and a'
+                    'figure image will be written (excluding file extensions).')
+args = parser.parse_args()
+
+
+world = World()
+import_jet_mesh(world, override_material=AbsorbingSurface())
+kb5v_collimator_mesh = import_obj(KB5V[0][0], scaling=0.001, parent=world, material=AbsorbingSurface())
+kb5h_collimator_mesh = import_obj(KB5H[0][0], scaling=0.001, parent=world, material=AbsorbingSurface())
+
+
+edge2d_sim = load_edge2d_from_eproc(args.sim_path)
+# edge2d_sim = load_edge2d_from_eproc('/common/cmg/jsimpson/edge2d/runs/run1708151A/tran')
+plasma = edge2d_sim.create_plasma(parent=world)
+plasma.atomic_data = OpenADAS(missing_rates_return_null=True)
+
+models = []
+for species in plasma.composition:
+    models.append(TotalRadiatedPower(species.element, species.charge))
+plasma.models = models
+
+
+plt.ion()
+plt.figure()
+xrange = np.linspace(0, 4, 200)
+yrange = np.linspace(-2, 2, 200)
+rad_rz_intensity = np.zeros((200, 200))
+direction = Vector3D(0, 1, 0)
+for i, x in enumerate(xrange):
+    for j, y in enumerate(yrange):
+        for model in models:
+            emission = model.emission(Point3D(x, 0.0, y), direction, Spectrum(650, 660, 1))
+            rad_rz_intensity[j, i] += emission.total()
+plt.imshow(rad_rz_intensity, extent=[1.5, 4, -2, 2], origin='lower')
+plt.colorbar()
+plt.xlabel('r axis')
+plt.ylabel('z axis')
+plt.title("Total radiated power in R-Z [W/m^3/str]")
+
+
+kb5v = load_kb5_camera('KB5V', parent=world)
+kb5v_measurements = np.zeros((len(kb5v)))
+for i, detector in enumerate(kb5v):
+    detector.observe()
+    kb5v_measurements[i] = detector.pipelines[0].value.mean
+
+plt.figure()
+plt.plot(kb5v_measurements)
+plt.xlabel('Detector Index')
+plt.ylabel('Measured Power [W]')
+plt.title('Measured powers for KB5V')
+
+kb5h = load_kb5_camera('KB5H', parent=world)
+kb5h_measurements = np.zeros((len(kb5h)))
+for i, detector in enumerate(kb5h):
+    detector.observe()
+    kb5h_measurements[i] = detector.pipelines[0].value.mean
+
+plt.figure()
+plt.plot(kb5h_measurements)
+plt.xlabel('Detector Index')
+plt.ylabel('Measured Power [W]')
+plt.title('Measured powers for KB5H')
+
+if args.output:
+    np.save(args.output + '_kb5v', kb5v_measurements)
+    np.save(args.output + '_kb5h', kb5h_measurements)
+
+plt.ioff()
+plt.show()
